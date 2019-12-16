dd9d2db0d5f16df384180596a7a674792cd01f42
Ahmed Hussein <wolktheline@yahoo.com>
2019-12-16 21:08:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 def key_for_min_value(name_hash)
   name_hash = {"chair"=>4 ,"table"=>5}
-  name_hash.each |chair , table|
+  name_hash.collect do |chair , table|
   if chair < table 
     puts chair
     elsif chair > table 
